56db0a5803478370607cbdf138b6b45f52c1ccc5
liar780@gmail.com <liar780@gmail.com>
2022-08-16 01:45:26 +0700

    fix css, add function backdoop

--- src/componen/Backdrop.js
+++ src/componen/Backdrop.js
@@ -1,4 +1,4 @@
-function Backdrop(){
-    return  <div className="Backdrop" />;
+function Backdrop(props){
+    return <div className="backdrop" onClick={props.onCancle} />;
 }
 export default Backdrop;
--- src/componen/Todo.js
+++ src/componen/Todo.js
@@ -9,6 +9,11 @@ function Todo(props) {
   function deleteHandler() {
     setModalIsOpen(true);
   }
+
+function closeModalHandler(){
+    setModalIsOpen(false);
+}
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -19,7 +24,7 @@ function Todo(props) {
       </div>
       {modalIsOpen && <Modal />} 
 
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && <Backdrop onCancle={closeModalHandler}/>}
     </div>
   );
 }
