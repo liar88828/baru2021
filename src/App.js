18a4e01436c8c9061e4d4c7203fde63a92c9879b
liar780@gmail.com <liar780@gmail.com>
2022-08-16 01:27:33 +0700

    fix import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Backdrop from "./componen/Backdrop";
-import Modal from "./componen/Modal";
 import Todo from "./componen/Todo";
 
 function App() {
@@ -9,8 +7,7 @@ function App() {
       <Todo text="learen react" />
       <Todo text="Master react" />
       <Todo text="Explore the full react course" />
-      <Modal />
-      <Backdrop/>
+    
     </div>
   );
 }
--- src/componen/Todo.js
+++ src/componen/Todo.js
@@ -1,15 +1,27 @@
-function Todo (props){
-    function deleteHandler(){
+import { useState } from "react";
 
-    }
-    return (
-      <div className="card">
-        <h2>{props.text}</h2>
-        <div className="action">
-          <button className="btn" onClick={deleteHandler}>Delete</button>
-        </div>
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
+function Todo(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModalIsOpen(true);
+  }
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="action">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
-    );
+      {modalIsOpen && <Modal />} 
+
+      {modalIsOpen && <Backdrop />}
+    </div>
+  );
 }
 
 export default Todo;
