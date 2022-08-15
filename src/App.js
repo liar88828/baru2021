1055c4383a7328108b359ec02685353fd7563b8d
liar780@gmail.com <liar780@gmail.com>
2022-08-16 00:26:39 +0700

    create todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import Todo from "./componen/Todo";
+
 function App() {
   return (
     <div>
       <h1>My todo</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="action">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+    <Todo />
     </div>
   );
 }
